9fa1a700e38156949acfaac79b6e51a72e4d6818
ajadreddy007 <ajad.edavelly@testaing.com>
2023-01-09 20:20:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { questions } from './components/Data';
 function App() {
 
   const [currentQuestion,setCurrentQuestion] = useState(0);
+
   const handleNext = () => {
     const nextQuestion = currentQuestion+1;
     if(nextQuestion<questions.length){
       setCurrentQuestion(nextQuestion);
   }
+
   }
   return (
     <div className="App">
       <h3>{questions[currentQuestion].questionText}</h3>
       {
-        questions[currentQuestion].answerOptions.map((answer)=>(
+        questions[currentQuestion].answerOptions.map((answer)=>( 
           <div>
-            <input type="radio" /> {answer.answerText}
+            <input type="radio" /> {answer.answerText} 
           </div>
         ))
       }
