9559ca3376a3e20aa399a5520d4da53d6abe7218
ajadreddy007 <ajad.edavelly@testaing.com>
2023-01-09 22:44:43 +0530

    implmented saving answers and displaying score

--- src/App.js
+++ src/App.js
@@ -4,25 +4,49 @@ import { questions } from './components/Data';
 function App() {
 
   const [currentQuestion,setCurrentQuestion] = useState(0);
+  const [score,setScore] = useState(0);
+  const [showScore,setShowScore] = useState(false); 
+  const [selectedOption,setSelectedoption] = useState(null);
 
-  const handleNext = () => {
-    const nextQuestion = currentQuestion+1;
-    if(nextQuestion<questions.length){
-      setCurrentQuestion(nextQuestion);
+  const saveNext = (answer) => {
+    setSelectedoption(answer);
   }
 
+  const handleNext = (selectedOption) => {
+    const nextQuestion = currentQuestion+1;
+    if(selectedOption){
+      if(selectedOption.isCorrect===true){
+        setScore(score+1);
+      }
+    }
+
+    if(nextQuestion<questions.length){
+      setCurrentQuestion(nextQuestion);
+    }
   }
   return (
     <div className="App">
-      <h3>{questions[currentQuestion].questionText}</h3>
-      {
-        questions[currentQuestion].answerOptions.map((answer)=>( 
-          <div>
-            <input type="radio" /> {answer.answerText} 
-          </div>
-        ))
+      {showScore?(
+        <h1>You scored {score} out of {questions.length}</h1>
+      )
+      :(
+        <>
+        <div className='qsn'>
+          <h3>{currentQuestion+1}. {questions[currentQuestion].questionText}</h3>
+        </div>
+        
+        {
+          questions[currentQuestion].answerOptions.map((answer)=>( 
+            <div onClick={()=>saveNext(answer)}>
+              <input name='a' type="radio" /> {answer.answerText} 
+            </div>
+          ))
+        }
+        <button onClick={()=>handleNext(selectedOption)}>Save and Next</button>
+        <button onClick={setShowScore} >Submit test</button>
+        </>
+      )
       }
-      <button onClick={handleNext} >Next</button>
     </div>
   );
 }
